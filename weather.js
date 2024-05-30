b10d2eddcffacb9f7f6d57b917d8fe9c735e6b87
G384272023 <238427@st.takushoku-u.ac.jp>
2024-05-30 15:06:40 +0900

    演習

--- weather.js
+++ weather.js
@@ -46,78 +46,78 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
-let b1 = document.querySelector('button#print');
-b1.addEventListener('click', iroiro);
-let w=document.querySelector('body');
- w.style.backgroundColor = '#E6E6FA';
-function iroiro() {
-  
-  let i = document.querySelector('input[name="basho"]');
-  let g=i.value;
-  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+g+'.json';
-  axios.get(url)
-  .then(showResult)   // 通信成功
-  .catch(showError)   // 通信失敗
-  .then(finish);      // 通信の最後の処理
-  
-  let r = Math.floor(Math.random() * 12)+1;
-  let body = document.querySelector('body');
-  if(r==1){
-    body.style.backgroundColor = '#ffbcbc';
-  }else if(r==2){
-    body.style.backgroundColor = '#ffbcdd';
-  }else if(r==3){
-    body.style.backgroundColor = '#ffbcff';
-  }else if(r==4){
-    body.style.backgroundColor = '#ddbcff';
-  }else if(r==5){
-    body.style.backgroundColor = '#bcbcff';
-  }else if(r==6){
-    body.style.backgroundColor = '#bcddff';
-  }else if(r==7){
-    body.style.backgroundColor = '#bcffff';
-  }else if(r==8){
-    body.style.backgroundColor = '#bcffdd';
-  }else if(r==9){
-    body.style.backgroundColor = '#bcffbc';
-  }else if(r==10){
-    body.style.backgroundColor = '#ddffbc';
-  }else if(r==11){
-    body.style.backgroundColor = '#fffbc';
-  }else if(r==12){
-    body.style.backgroundColor = '#ffddbc';
-  }
-  
-}
-function showResult(resp){
-  let data = resp.data;
-  if (typeof data === 'string') {
-    data = JSON.parse(data);
-  }
-  console.log(data);
-  console.log(data.name);
-  console.log(data.main.temp_max);
-  console.log(data.main.temp_min);
-  let p1 = document.querySelector('p#name');
-p1.textContent = ("都市名: "+data.name+"🏙️"); 
-let p2 = document.querySelector('p#temp_max');
-p2.textContent = ("最高気温: "+data.main.temp_max+"℃ 🥵");
-let p3 = document.querySelector('p#temp_min');
-p3.textContent = ("最低気温: "+data.main.temp_min+"℃ 🥶");
-let p4= document.querySelector('p#situ');
-p4.textContent = ("湿度: "+data.main.humidity+"% 💦");
-
+console.log(data.name);
+for(let a of "name"){
+  console.log(a.name);
 }
 
+let btn = document.querySelector('button#botton');
+btn.addEventListener('click', hantei);
+
+function hantei() {
+  let s= document.querySelector('select#mati');
+  let idx = s.selectedIndex; 
+
+  let os = s.querySelectorAll('option'); 
+  let o = os.item(idx);      
+
+    let kensaku=o.value;
+    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/"+kensaku+".json";
+    console.log(url);
+    
+
+  
+
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
+    
+}
+
+function showResult(resp) {
+    
+    let data = resp.data;
+
+    
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    
+    let tenki1=document.querySelector("span#a1");
+    tenki1.textContent=data.name;
+    let tenki2=document.querySelector("span#a2");
+    tenki2.textContent=data.weather[0].description;
+    let tenki3=document.querySelector("span#a3");
+    tenki3.textContent=data.wind.speed;
+    let tenki4=document.querySelector("span#a4");
+    tenki4.textContent=data.wind.deg;
+    let tenki5=document.querySelector("span#a5");
+    tenki5.textContent=data.main.temp_max;
+    let tenki6=document.querySelector("span#a6");
+    tenki6.textContent=data.main.temp_min;
+    let tenki7=document.querySelector("span#a7");
+    tenki7.textContent=data.main.temp;
+    let tenki9 = document.querySelector("span#a9");
+    tenki9.textContent=data.main.humidity;
+}
+
+// 通信エラーが発生した時の処理
 function showError(err) {
-  console.log(err);
+    console.log(err);
 }
+
+// 通信の最後にいつも実行する処理
 function finish() {
-  console.log('Ajax 通信が終わりました');
+    console.log('Ajax 通信が終わりました');
 }
+
 console.log(data.name);
 console.log(data.weather[0].description);
 console.log(data.main.temp_max);
 console.log(data.main.temp_min);
 console.log(data.wind.deg);
 console.log(data.wind.speed);
+
+
